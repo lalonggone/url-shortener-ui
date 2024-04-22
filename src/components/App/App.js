a9df022699e6af9cacc8c55524f86636ee4cf496
Laura Long <lauralonggone@gmail.com>
2024-04-22 09:29:04 -0600

    Change urls props to an empty array

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { getUrls } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
-import UrlForm from '../UrlForm/UrlForm';
+import UrlForm from '../UrlForm/UrlForm'
 
 function App () {
   const [urls, setUrls] = useState([]);
@@ -17,10 +17,9 @@ function App () {
         <h1>URL Shortener</h1>
         <UrlForm />
       </header>
-
-      <UrlContainer urls={"<<<Urls should go here>>>"}/>
+      <UrlContainer urls={[]}/>
     </main>
-  );
+  )
 }
 
 export default App;
