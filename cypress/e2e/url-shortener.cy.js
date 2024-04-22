04ca84ef931c0e48543beca87270380103e4aeb3
Laura Long <lauralonggone@gmail.com>
2024-04-22 11:42:01 -0600

    Test display after POST requeset with stubbed data

--- cypress/e2e/url-shortener.cy.js
+++ cypress/e2e/url-shortener.cy.js
@@ -4,22 +4,15 @@ describe('URL Shortener', () => {
       statusCode: 200,
       body: {
         urls: [
-
-          // these links dont match ...
           {
             id: 1,
-            long_url: 'https://images.unsplash.com/photo-1560807707-8cc77767d783',
+            long_url:
+              'https://images.squarespace-cdn.com/content/v1/5abef86f7e3c3a242ca0bd46/7bb5d762-74c3-445b-97d3-4b40f5038e7b/0Y2A6620.jpg',
             short_url: 'http://localhost:3001/useshorturl/1',
-            title: 'Awesome photo'
+            title: 'Supposed to be a frenchie',
           },
-          {
-            id: 2,
-            long_url: 'https://images.unsplash.com/photo-1560807707-8cc77767d783',
-            short_url: 'http://localhost:3001/useshorturl/2',
-            title: 'Another awesome photo'
-          }
-        ]
-      }
+        ],
+      },
     }).as('getUrls')
 
     cy.visit('http://localhost:3000')
@@ -27,9 +20,37 @@ describe('URL Shortener', () => {
   })
 
   it('should display the existing shortened URLs', () => {
-    cy.get('section')
-      .children()
-      .should('have.length', 2)
+    cy.get('section').children().should('have.length', 1)
+    cy.get('section > :nth-child(1)').should('contain', 'Supposed to be a frenchie')
+    cy.get('section > :nth-child(1)').should('contain', 'http://localhost:3001/useshorturl/1')
+    cy.get('section > :nth-child(1)').should('contain', 'https://images.squarespace-cdn.com/content/v1/5abef86f7e3c3a242ca0bd46/7bb5d762-74c3-445b-97d3-4b40f5038e7b/0Y2A6620.jpg')
   })
 
+  it('should be able to shorten a URL', () => {
+    cy.intercept('POST', 'http://localhost:3001/api/v1/urls', {
+      statusCode: 201,
+      body: {
+        id: 2,
+        long_url:
+          'https://www.reddit.com/r/nasa/comments/1bu69i7/how_astronauts_cut_their_hair_on_the/',
+        short_url: 'http://localhost:3001/useshorturl/2',
+        title: 'Astronaut haircut',
+      },
+    }).as('postUrl')
+
+    cy.get('input[name="title"]')
+      .type('Astronaut haircut')
+      .get('input[name="urlToShorten"]')
+      .type(
+        'https://www.reddit.com/r/nasa/comments/1bu69i7/how_astronauts_cut_their_hair_on_the/'
+      )
+      .get('button')
+      .click()
+      .wait('@postUrl')
+
+    cy.get('section').children().should('have.length', 2)
+    cy.get('section > :nth-child(2)').should('contain', 'Astronaut haircut')
+    cy.get('section > :nth-child(2)').should('contain', 'http://localhost:3001/useshorturl/2')
+    cy.get('section > :nth-child(2)').should('contain', 'https://www.reddit.com/r/nasa/comments/1bu69i7/how_astronauts_cut_their_hair_on_the/')
+  })
 })
